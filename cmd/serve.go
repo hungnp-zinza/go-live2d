126fe31b007a1d35d3662b858e3e0ae03d35b073
hungnp <hung.np@zinza.com.vn>
2024-01-09 07:18:13 +0700

    init

---
+++ cmd/serve.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func Serve() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/models", getModels)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func getModels(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
---
+++ go-live2d.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/hungnp-zinza/go-live2d/cmd"
+
+func main() {
+	cmd.Serve()
+}
